b9c53399be6ab6cf9ad7f08e6f56fb0d958c8902
syf1844803351 <122741327+syf1844803351@users.noreply.github.com>
2025-06-10 14:58:12 +0800

    Add files via upload

---
+++ src/scripts/beauty.sh
@@ -0,0 +1,2 @@
+cd ..
+python main.py --data_name Beauty --rec_weight 1. --alpha 0.1 --beta 0.1 --f_neg --intent_num 1024 --hidden_dropout_prob 0.5 --attention_probs_dropout_prob 0.5 --p 13
---
+++ src/scripts/sports.sh
@@ -0,0 +1,2 @@
+cd ..
+python main.py --data_name Sports_and_Outdoors --rec_weight 1. --alpha 0.1 --beta 0.1 --f_neg --intent_num 512 --hidden_dropout_prob 0.5 --attention_probs_dropout_prob 0.5 --p 7
